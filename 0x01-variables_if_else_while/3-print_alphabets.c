74bb2170a33ade5eeef18e09a04c1d92334f4d48
Rediet Yaregal <115864564+ReduYT@users.noreply.github.com>
2023-08-11 15:55:12 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,13 @@
   */
 int main(void)
 {
-	for (char i = 'a'; i < 'z'; ++i)
+	char i;
+	
+	for (i = 'a'; i <= 'z'; ++i)
 	{
 		putchar(i);
 		putchar(i - 32);
-		putchar("\n");
 	}
+		putchar('\n');
 	return (0);
 }
