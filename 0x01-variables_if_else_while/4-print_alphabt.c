ed777f5f1f49bdada8f2080eff90358a7981463d
ReduYT <yaregalrediet10141005@gmail.com>
2023-08-13 11:57:01 +0300

    Added task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - my main function
+ *
+ * program outputs a message
+ *
+ *Return: returns 0 everytime
+  */
+int main(void)
+{
+	char i;
+
+	for (i = a; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - my main function
+ *
+ * program outputs a message
+ *
+ *Return: returns 0 everytime
+  */
+int main(void)
+{
+	char i;
+
+	for (i = z; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
