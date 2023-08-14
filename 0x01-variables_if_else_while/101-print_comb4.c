4ed7ed0e6ddb91339184348b98d940477fc0fbf0
ReduYT <yaregalrediet10141005@gmail.com>
2023-08-14 17:49:59 +0300

    Added task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *main - my main function
+ *
+ * program outputs a message
+ *
+ *Return: returns 0 everytime
+  */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
