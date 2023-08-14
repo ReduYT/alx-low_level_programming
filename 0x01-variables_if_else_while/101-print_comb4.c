5699be2f59ba505fdf9e325560e136d46acead36
ReduYT <yaregalrediet10141005@gmail.com>
2023-08-14 18:17:05 +0300

    Updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,11 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				if (i != 9)
+				if (i == 7 && j == 8 && k == 9)
+				{
+					putchar('\n');
+				}
+				else
 				{
 					putchar(',');
 					putchar(' ');
