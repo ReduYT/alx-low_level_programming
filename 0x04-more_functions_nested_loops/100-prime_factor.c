f0ae29cc17a212b2c97bfec461c2c5a8503f1de8
ReduYT <yaregalrediet10141005@gmail.com>
2023-08-17 23:54:21 +0300

    Added function files and updated main files

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int x = 2;
+	long n = 612852475143;
+
+	while (x != n)
+	{
+		if (n % x == 0)
+		{
+			n = n / x;
+		} 
+		else
+		{
+			x++;
+		}
+	}
+	printf("%ld\n", n);
+	return (0);
+}
