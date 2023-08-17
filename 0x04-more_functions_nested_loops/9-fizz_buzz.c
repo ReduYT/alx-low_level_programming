6415275d582ecc06af1c868bb720ceab024856ae
Rediet Yaregal <115864564+ReduYT@users.noreply.github.com>
2023-08-17 19:20:33 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,12 @@ int main(void)
 		if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (num != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
