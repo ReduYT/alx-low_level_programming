304c87e6e2b169dc8cfaecc703db00d203adfce5
ReduYT <yaregalrediet10141005@gmail.com>
2023-08-17 17:31:43 +0300

    Added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i;
+		int j;
+
+		for (i = size-1; i >= 0; i--)
+		{
+			for (j = i; j >= 0;  j--)
+			{
+				putchar(' ');
+			}
+			for (int k = i; k < size-1; k++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
