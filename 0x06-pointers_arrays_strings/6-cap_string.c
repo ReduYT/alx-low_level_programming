b056ffcfa09fb375cf5744338cfef9ed35b16889
Rediet Yaregal <115864564+ReduYT@users.noreply.github.com>
2023-08-25 23:39:59 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,8 @@
  */
 char *cap_string(char *str)
 {
-	for (int i = 0; i < strlen(str); i++)
+	int i = 0;
+	while (str[i]++)
 	{
 		if ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 		     str[i] == ',' || str[i] == ';' || str[i] == '.' ||
