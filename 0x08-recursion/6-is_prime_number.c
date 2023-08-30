57a4242471d4908bc63b0d7599f29acfa47c90f0
Rediet Yaregal <115864564+ReduYT@users.noreply.github.com>
2023-08-30 22:39:45 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,15 @@
  */
 int is_prime_number(int n)
 {
-	return (is_prime_recursive(n, 2));
+        return (is_prime_recursive(n, 2));
 }
 int is_prime_recursive(int n, int divisor)
 {
-	if (n <= 1)
-		return (0);
-	if (divisor * divisor > n)
-		return (1);
-	if (n % divisor == 0)
-		return (0);
-	return (is_prime_recursive(n, divisor + 1));
+        if (n <= 1)
+                return (0);
+        if (divisor * divisor > n)
+                return (1);
+        if (n % divisor == 0)
+                return (0);
+        return (is_prime_recursive(n, divisor + 1));
 }
