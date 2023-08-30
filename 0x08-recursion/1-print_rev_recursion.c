c5adea3dfc1ee082961df5ce33a69cb3876dcf8e
ReduYT <yaregalrediet10141005@gmail.com>
2023-08-30 22:30:08 +0300

    Added

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a reverse of string using recursion
+ * @s: The string to be printed
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * is_prime_recursive - Helper function for
+ * checking if a number is prime re * cursively.
+ * @n: The number to be checked.
+ * @divisor: The current divisor being tested.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_recursive(n, 2));
+}
+int is_prime_recursive(int n, int divisor)
+{
+	if (n <= 1)
+		return (0);
+	if (divisor * divisor > n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_recursive(n, divisor + 1));
+}
