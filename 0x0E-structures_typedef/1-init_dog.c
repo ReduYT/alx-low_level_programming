6051102f769b9e08f5c9a41946c6f8c11073b1c8
ReduYT <yaregalrediet10141005@gmail.com>
2023-09-11 23:20:15 +0300

    Added

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes members of dog
+ * @d: pointer to dog
+ * @name: dog name to be assigned
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age
+	}
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - prints dog  info
+*@d: points to dog
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s", d->name);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s", d->owner);
+	}
+	else
+		return;
+}
